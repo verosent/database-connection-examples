ad6ac79fc342780d34b82ac7f45a80f8f09dd272
Manuel Schnitzer <manuel.schnitzer@verosent.com>
2020-10-15 17:51:01 +0200

    Add a comment where to find the chain file

--- php/show_tables.php
+++ php/show_tables.php
@@ -2,6 +2,10 @@
  $mysqli = mysqli_init();
 
  $mysqli->options(MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, true);
+
+ // get a copy of our Certificate Authority's chain file from ...
+ // https://github.com/verosent/certificate-authority/blob/main/chain.pem
+ // ... and store it locally!
  $mysqli->ssl_set(null, null, '/path/to/verosent/chain.pem', null, null);
 
  if ($mysqli->real_connect('DATABASE_SERVER', 'DATABASE_USER', 'DATABASE_PASSWORD', 'DATABASE_NAME')) {
